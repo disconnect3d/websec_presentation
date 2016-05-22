dd926841fa82ef32a789d34bf1a4580bfa8e4da6
Dominik Czarnota <dominik.b.czarnota@gmail.com>
2016-05-22 10:37:27 +0200

    Added csrf hidden post example

--- examples/3.csrf/csrf_serv.py
+++ examples/3.csrf/csrf_serv.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 
-from flask import Flask, render_template, url_for, redirect
-
+from flask import Flask, render_template, url_for, redirect, request, make_response
+from wtforms import Form, IntegerField, validators
 
 app = Flask(__name__)
 
@@ -19,6 +19,24 @@ def vote():
     global votes
     votes += 1
     return redirect(url_for('index'))
+
+
+class AdminVoteForm(Form):
+    number = IntegerField('Number of votes', [validators.Required()])
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    form = AdminVoteForm(request.form)
+    
+    if request.method == 'POST' and form.validate() and request.cookies.get('Authorization') == 'tajne_haslo':
+        global votes
+        votes += form.number.data
+        return redirect(url_for('admin'))
+    
+    resp = make_response(render_template('admin_vote.html', form=form, votes=votes))
+    resp.set_cookie('Authorization', value='tajne_haslo')
+    return resp
 
 
 if __name__ == '__main__':  
